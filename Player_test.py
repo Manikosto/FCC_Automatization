ade82b20858588cbb9cddeeeb9b5c92ed9ff5053
Alexey <47078622+Manikosto@users.noreply.github.com>
2019-04-30 14:23:46 +0300

    Update Player_test.py

--- Player_test.py
+++ Player_test.py
@@ -13,7 +13,7 @@ import random
 
 class LP_tests(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome('chromedriver.exe')  # Обьявление драйвера
+        self.driver = webdriver.Chrome('chromedriver.exe') 
 
     def test_player(self):
         driver = self.driver
@@ -25,19 +25,19 @@ class LP_tests(unittest.TestCase):
         count_of_good = 0
         count_of_video = 0
         action = ActionChains(driver)
-        # Логинимся на странице
+        # Login on page
         driver.get('https://qa-www.freeconferencecall.com/login#login')
         driver.find_element(By.XPATH, "/html//input[@id='login_email']").send_keys('mkrtkv@gmail.com')
         driver.find_element(By.XPATH, "/html//input[@id='password']").send_keys('qwe123321ewq')
         driver.find_element(By.XPATH, "/html//button[@id='loginformsubmit']").click()
 
-        # Перешли в H&R
+        # Go to H&R
         time.sleep(10)
         driver.find_element(By.XPATH, "//div[@id='logged-in']/div/div[2]/ul/li[2]/a[1]").click()
         time.sleep(10)
         driver.find_element(By.XPATH, "//div[@id='logged-in']/div/div[2]/ul/li[2]/ul[@role='menu']//a[@href='/profile/history']").click()
         time.sleep(5)
-        # Выбрали тип конференции
+        # Choose type of conference
         element = driver.find_element(By.XPATH, "/html//div[@id='main']/div[@class='ember-view']/div[@class='history-record wall-section']//div[@class='col-xs-12']/div[@class='ember-view']/div/div/div[4]/div[@class='form-group']/div[@class='ember-view']/select")
         drp = Select(element)
         drp.select_by_index(1)
@@ -50,24 +50,24 @@ class LP_tests(unittest.TestCase):
 
         time.sleep(5)
 
-        # Переход по странице
+        # Click to the next page
         driver.find_element(By.XPATH, "/html//div[@id='main']/div[@class='ember-view']//nav[@class='ember-view']/ul/li[6]/a[@href='#']")
         next = driver.find_element(By.XPATH, "//li/a[text()='»']")
 
-        # Перебор записей
+        # Cycle of records
         while True:
             
             driver.refresh()
             driver.execute_script("window.scrollTo(0, -1000)")
             time.sleep(10)
 
-        # Получаем число видео-конференций
+        # Get count of video
             count = driver.find_elements(By.XPATH, "//i[@class='fa fa-lg fa-film']")
             # print(len(count))
             for i in range(1, len(count)):
                 elem = driver.find_element(By.XPATH, f"(//i[@class='fa fa-lg fa-film'])[{i}]")
 
-                # Ловим возможную ошибку по перекрытию иконки
+                # Try get error
                 try:
                     wait.until(EC.element_to_be_clickable((By.XPATH, "//i[@class='fa fa-lg fa-film']")))
                     elem.click()
@@ -75,10 +75,10 @@ class LP_tests(unittest.TestCase):
                     print('Element is not clickable')
 
 
-                # Переключаемся на IFRAME ошибки
+                # Switch to the IFRAME of Error
                 driver.switch_to.frame(driver.find_element_by_xpath("//iframe[@class='embed-responsive-item']"))
 
-                # Ловим ошибку
+                # Get error
                 try:
                     wait.until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Reload player']")))
                     count_of_errors += 1
@@ -88,20 +88,18 @@ class LP_tests(unittest.TestCase):
                     count_of_good += 1
                 finally:
                     driver.switch_to.default_content()
-                # time.sleep(5)
                 driver.find_element(By.XPATH, "//i[@title='Close']").click()
             next = driver.find_element(By.XPATH, "//a[text()='»']/ancestor::li")
-            # print(next.get_attribute('class'))
 
-            # Стопим цикл если страницы кончились
+            # Stop cycle
             if next.get_attribute("class") == 'next disabled':
                 break
 
-            # Обходим первую страницу
+            # Void first page
             if driver.find_element(By.XPATH, "//a[text()='1']/ancestor::li").get_attribute('class') != 'active':
                 next.find_element_by_xpath('a').click()
 
-        # Отчет
+        # Result
         count_of_video += int(count_of_errors) + int(count_of_good)
         print('Count of record: ' + str(count_of_video))
         print('Errors: ' + str(count_of_errors))
